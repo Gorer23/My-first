8de82d4c494fb71f8f716a0d8827df681896cf62
Igor <igorkrsd093@gmail.com>
2024-01-12 13:02:12 +0200

    First report

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict"; //только в начале,помогает ликвидировать ошибки в коде
+
+// alert(1);
+
+// const result = confirm("Are you here");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer);
+
+const answers = [];
+
+answers[0]= prompt("What's your name?", "");
+answers[1]= prompt("What's your second name?", "");
+answers[2]= prompt("What's your age?", "");
+
+document.write(answers);  //устаревшая тема для тестов
+
+console.log(null);
