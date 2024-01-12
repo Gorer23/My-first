e20a27d1e4a55ce932aaa078e8ee3d3a6a2c49cc
Igor <igorkrsd093@gmail.com>
2024-01-12 13:25:50 +0200

    text

--- script.js
+++ script.js
@@ -8,12 +8,45 @@
 // const answer = prompt("Вам есть 18?", "18");
 // console.log(answer);
 
-const answers = [];
+// const answers = [];
 
-answers[0]= prompt("What's your name?", "");
-answers[1]= prompt("What's your second name?", "");
-answers[2]= prompt("What's your age?", "");
+// answers[0]= prompt("What's your name?", "");
+// answers[1]= prompt("What's your second name?", "");
+// answers[2]= prompt("What's your age?", "");
 
-document.write(answers);  //устаревшая тема для тестов
+// document.write(answers);  //устаревшая тема для тестов
 
-console.log(null);
+// console.log(null);
+
+// const numberOfFilms=[];
+// numberOfFilms[0]= prompt("how many film your watch?", "");
+// console.log(numberOfFilms[0]);
+
+const numberOfFilms = +prompt("How many films have you watched?", "");
+console.log(numberOfFilms);
+let personalMovieDB = {
+    count:(numberOfFilms), // Convert the input to a number using parseInt
+     movies: {},
+     actors: {},
+    genres: {},
+    privat: false
+     }
+     const a =prompt("What's your last film?", ""),
+           b=  prompt("What's your rating?", "");
+           personalMovieDB.movies[a]=a;
+           personalMovieDB.movies[b]=b;
+           console.log(personalMovieDB);
+     
+// const numberOfFilms2 = [];
+// numberOfFilms2[0] = prompt("What's your last film?", "");
+// numberOfFilms2[1] = prompt("What's your rating?", "");
+
+// let personalMovieDB = {
+//     count:(numberOfFilms), // Convert the input to a number using parseInt
+//     movies: [numberOfFilms2[0],numberOfFilms2[1]],
+//     actors: {},
+//     genres: {},
+//     privat: false
+// };
+
+// console.log(personalMovieDB.movies);
